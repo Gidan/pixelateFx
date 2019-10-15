6fcb33412ea08bdd2d56e19f0656e8f01678b096
Andrea Amatucci <amatucci.andrea@gmail.com>
2019-10-15 23:12:20 +0200

    removed sample image. added filechooser

--- src/main/java/com/aamatucci/pixelatefx/Pixelate.java
+++ src/main/java/com/aamatucci/pixelatefx/Pixelate.java
@@ -5,25 +5,34 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import java.io.File;
 
 public class Pixelate extends Application {
+
+    private Stage stage;
 
     public static void main(final String[] args) {
         Application.launch(args);
     }
 
     @FXML
-    private
-    Slider slider;
+    private Slider slider;
 
     @FXML
-    private
-    PixelImageView pixelImageView;
+    private PixelImageView pixelImageView;
+
+    @FXML
+    private Button btnFileChooser;
+
+    final FileChooser fileChooser = new FileChooser();
 
     public void start(final Stage primaryStage) throws Exception {
+        stage = primaryStage;
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/Pixelate.fxml"));
         fxmlLoader.setController(this);
         Parent root = fxmlLoader.load();
@@ -35,9 +44,22 @@ public class Pixelate extends Application {
 
     @FXML
     private void initialize() {
-        final Image originalImage = new Image(this.getClass().getResource("/clouds.jpg").toExternalForm());
         pixelImageView.pixelSizeProperty().bind(slider.valueProperty());
-        pixelImageView.setImage(originalImage);
+        btnFileChooser.setOnMouseClicked(event -> this.openFileChooser());
+
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.jpg", "*.png"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+    }
+
+    private void openFileChooser() {
+        File file = fileChooser.showOpenDialog(this.stage);
+        if (file != null) {
+            Image image = new Image(file.toURI().toString());
+            pixelImageView.setImage(image);
+        }
     }
 
 
